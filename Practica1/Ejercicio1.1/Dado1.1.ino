e66846c02fe424a290728ef1a7e22a2346bc221d
alejandroleon98 <alexleon8898@gmail.com>
2020-09-20 11:15:52 +0200

    Formatear

--- Practica1/Ejercicio1.1/Dado1.1.ino
+++ Practica1/Ejercicio1.1/Dado1.1.ino
@@ -9,49 +9,58 @@ int led1 = 2;
 int led2 = 3;
 int led3 = 4;
 
-int boton = 5;
-int pulsado = 0;
+int botonDado = 5;
+int botonPulsado = 0;
 
-void setup() {
+void setup()
+{
+
+  //Semilla
+  randomSeed(analogRead(pinForRandom));
   Serial.begin(9600);
- 
+
+  //Leds
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
-  
-  pinMode(boton, INPUT);
-  
-  randomSeed(analogRead(pinForRandom));
+
+  //botonDado
+  pinMode(botonDado, INPUT);
 }
 
-void loop() {
+void loop()
+{
 
-  int lectura = digitalRead(boton);
-  
-  if(lectura == HIGH && pulsado == 0){
-    pulsado = 1;
+  //Leer botonDado
+  int leer = digitalRead(botonDado);
 
-    switch(random(3)){
-      case 0:
-        digitalWrite(led1, HIGH);
-        digitalWrite(led2, LOW);
-        digitalWrite(led3, LOW);
+  //Si el usuario ha botonPulsado el botonDado
+  if (botonPulsado == 0 && leer == HIGH)
+  {
+
+    botonPulsado = 1;
+
+    switch (random(3))
+    {
+    case 0:
+      digitalWrite(led1, HIGH);
+      digitalWrite(led2, LOW);
+      digitalWrite(led3, LOW);
       break;
-      case 1:
-        digitalWrite(led1, HIGH);
-        digitalWrite(led2, HIGH);
-        digitalWrite(led3, LOW);
+    case 1:
+      digitalWrite(led1, HIGH);
+      digitalWrite(led2, HIGH);
+      digitalWrite(led3, LOW);
       break;
-      case 2:
-        digitalWrite(led1, HIGH);
-        digitalWrite(led2, HIGH);
-        digitalWrite(led3, HIGH);
+    case 2:
+      digitalWrite(led1, HIGH);
+      digitalWrite(led2, HIGH);
+      digitalWrite(led3, HIGH);
       break;
     }
-    
   }
-  else if(lectura == LOW){
-    pulsado = 0;
+  else if (leer == LOW)
+  {
+    botonPulsado = 0;
   }
-  
 }
